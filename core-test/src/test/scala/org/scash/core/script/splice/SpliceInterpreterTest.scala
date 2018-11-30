869b2ef499f58763722b9f96060dae8e13b7fed7
floreslorca <iratxo.floreslorca@gmail.com>
2018-11-30 17:38:28 -0600

    add split tests

--- core-test/src/test/scala/org/scash/core/script/splice/SpliceInterpreterTest.scala
+++ core-test/src/test/scala/org/scash/core/script/splice/SpliceInterpreterTest.scala
@@ -6,13 +6,12 @@ package org.scash.core.script.splice
  */
 import org.scash.core.script.{ ExecutedScriptProgram, ScriptProgram }
 import org.scash.core.script.constant._
-import org.scash.core.script.result.{ ScriptError, ScriptErrorInvalidStackOperation, ScriptErrorPushSize }
+import org.scash.core.script.result.{ ScriptErrorInvalidStackOperation, ScriptErrorPushSize }
 import org.scash.core.util.{ BitcoinSUtil, TestUtil }
 import org.scalatest.{ FlatSpec }
 import org.scash.core.TestHelpers
 import org.scash.core.consensus.Consensus
 
-import scodec.bits._
 import scodec.bits.ByteVector
 
 class SpliceInterpreterTest extends FlatSpec with TestHelpers {
@@ -25,7 +24,6 @@ class SpliceInterpreterTest extends FlatSpec with TestHelpers {
     val newProgram = SI.opSize(program)
     newProgram.stack must be(List(OP_0, OP_0))
     newProgram.script.isEmpty must be(true)
-
   }
 
   it must "determine the size of script number 0 correctly" in {
@@ -82,18 +80,6 @@ class SpliceInterpreterTest extends FlatSpec with TestHelpers {
   def str2ByteVector(str: String) =
     BitcoinSUtil.decodeHex(BitcoinSUtil.flipEndianness(ByteVector(str.getBytes)))
 
-  it must "evaluate an OP_CAT with empty elements correctly" in {
-    val r = str2ByteVector("myString")
-
-    val stack = List(ScriptConstant(r), ScriptConstant.empty)
-    val stack2 = List(ScriptConstant.empty, ScriptConstant(r))
-    val script = List(OP_CAT)
-
-    List(stack, stack2)
-      .map(ScriptProgram(TestUtil.testProgramExecutionInProgress, _, script))
-      .map(SI.opCat).map(_.stack.head.bytes must be(r))
-  }
-
   it must "evaluate an OP_CAT that is bigger than `maxScriptElementSize` and fail with ScriptErrorPushSize" in {
     val stack = List(
       ScriptConstant(ByteVector.fill(Consensus.maxScriptElementSize)(1)),
@@ -104,15 +90,15 @@ class SpliceInterpreterTest extends FlatSpec with TestHelpers {
   }
 
   val inputs = List(
-    (ScriptConstant.empty, ScriptConstant.empty, ScriptConstant.empty),
-    (ScriptConstant.zero, ScriptConstant.zero, ScriptConstant(hex"0x0000")),
-    (ScriptConstant("0xab"), ScriptConstant("0xcd"), ScriptConstant("0xabcd")),
-    (ScriptConstant("0xabcdef"), ScriptConstant("0x12345678"), ScriptConstant("0xabcdef12345678")))
+    (ScriptConstant.empty, ScriptConstant.empty),
+    (ScriptConstant.zero, ScriptConstant.zero),
+    (ScriptConstant("0xab"), ScriptConstant("0xcd")),
+    (ScriptConstant("0xabcdef"), ScriptConstant("0x12345678")))
 
   it must "evaluate all OP_CAT successfully" in {
     inputs.map {
-      case (a, b, ex) =>
-        checkBinaryOp(a, b, OP_CAT, List(ex))(SI.opCat)
+      case (a, b) =>
+        checkBinaryOp(a, b, OP_CAT, List(ScriptConstant(a.bytes ++ b.bytes)))(SI.opCat)
 
         //Check empty concats
         checkBinaryOp(a, ScriptConstant.empty, OP_CAT, List(a))(SI.opCat)
@@ -121,4 +107,17 @@ class SpliceInterpreterTest extends FlatSpec with TestHelpers {
         checkBinaryOp(ScriptConstant.empty, b, OP_CAT, List(b))(SI.opCat)
     }
   }
+
+  it must "evaluate all OP_SPLIT successfully" in {
+    inputs.map {
+      case (a, b) =>
+        SI.opSplit(
+          ScriptProgram(
+            TestUtil.testProgramExecutionInProgress,
+            List(ScriptNumber(a.size), ScriptConstant(a.bytes ++ b.bytes)),
+            List(OP_SPLIT)))
+          .stack must be(List(a, b).reverse)
+    }
+  }
+
 }
